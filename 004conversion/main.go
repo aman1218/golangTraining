85114e3db290171c178cb19c14b02f75d8c878b1
aman1218 <amanprasad7468@gmail.com>
2023-06-26 17:14:27 +0530

    type conversion example

---
+++ 004conversion/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("enter any number")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("your input is", input)
+	incinput, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil{
+		fmt.Println(err)
+	}else{
+		fmt.Println("your incremented input is", incinput+1)
+	}
+}
