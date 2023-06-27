4692545e2307acb942ddf5084fd240983e9ef2b5
aman1218 <amanprasad7468@gmail.com>
2023-06-27 12:27:11 +0530

    array example

---
+++ 007arrays/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var arr [7]int
+	arr[0] = 25
+	arr[1] = 15
+	arr[2] = 19
+	arr[3] = 29
+	arr[4] = 38
+	arr[5] = 85
+	arr[6] = 77
+	fmt.Println("elements of array are:", arr)
+	fmt.Println("length of array is:", len(arr))
+}
