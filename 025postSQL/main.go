3462e44d0430bf913511d756701f18569fba8ad7
apKloudone <aman@kloudone.com>
2023-07-03 18:30:51 +0530

    database read, write, delete example update

--- 025postSQL/main.go
+++ 025postSQL/main.go
@@ -24,7 +24,7 @@ func main() {
 		age   int
 	}
 
-	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	postgresqlDbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", postgresqlDbInfo)
 	checkErr(err)
 	err = db.Ping()
